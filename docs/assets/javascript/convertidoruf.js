6d6df9375f9e0b113d97656441b5dedd54955ca5
Hugo M <hg.moraga@gmail.com>
2022-02-07 23:02:04 -0300

    ADD: Get UF and Convert CLP

--- docs/assets/javascript/convertidoruf.js
+++ docs/assets/javascript/convertidoruf.js
@@ -1,12 +1,28 @@
-!async function(){
+async function getUf() {
 
-    let uf = fetch('https://mindicador.cl/api/uf')
-        .then(result => result.json())
-        .then((output) => {
-            return uf = output.serie[0].valor;
-        
-            
-    }).catch(err => console.error(err));
-    
-    }();
-    
+    let response = await fetch('https://mindicador.cl/api/uf');
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+    let data = await response.json()
+    const uf = parseInt(data.serie[0].valor) 
+    return uf;
+
+}
+
+const uf = getUf();
+
+async function currencyConverter(source, valNum, uf) {
+  uf = await getUf();
+  valNum = parseFloat(valNum);
+  var inputUf = document.getElementById("inputUf");
+  var inputPesos = document.getElementById("inputPesos");
+  if (source == "inputUf") {
+    inputPesos.value = (valNum * uf).toFixed(0);
+  }
+  if (source == "inputPesos") {
+    inputUf.value = (valNum / uf).toFixed(0);
+  }
+}
+
+currencyConverter();
